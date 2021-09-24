d27fabd91eb61202b74ebfe70db43df9a992dd72
unn3daby <91330648+unn3daby@users.noreply.github.com>
2021-09-25 00:22:10 +0300

    Add files via upload

---
+++ Lab4/task3.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+int main() {
+	using namespace std;
+	float sr, a, b;
+	cin >> a >> b;
+	sr = (a + b) / 2;
+	cout << sr;
+}
---
+++ Lab4/task5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+int main()
+{
+    using namespace std;
+    float a, b;
+    cin >> a >> b;
+    cout << "sum = " << a + b << endl << "raznost' = " << a - b << endl << "proizved = " << a * b << endl << "chast moduley = " << abs(a) / abs(b) << endl;
+}
