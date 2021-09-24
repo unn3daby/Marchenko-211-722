0422e6acdbd80fd3235f0681b3d874f65fd1f7a7
unn3daby <91330648+unn3daby@users.noreply.github.com>
2021-09-25 00:20:43 +0300

    Add files via upload

---
+++ Lab1/task1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+	using namespace std;
+	int a, b;
+	cin >> a >> b;
+	cout << a * b << "-s" << endl << 2 * (a + b) << "-d";
+}
---
+++ Lab1/task2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+	using namespace std;
+	float L, pi = 3.14;
+	int d;
+	cin >> d;
+	L = d * pi;
+
+	cout << L;
+}
