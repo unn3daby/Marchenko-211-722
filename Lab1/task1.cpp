6ade1856c8f8f1174b0eb17dec3bca9682a09d84
unn3daby <91330648+unn3daby@users.noreply.github.com>
2021-09-25 00:21:54 +0300

    Delete Lab1 directory

--- Lab1/task1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-int main() {
-	using namespace std;
-	int a, b;
-	cin >> a >> b;
-	cout << a * b << "-s" << endl << 2 * (a + b) << "-d";
-}
--- Lab1/task3.cpp
+++
@@ -1,8 +0,0 @@
-# include <iostream>
-int main() {
-	using namespace std;
-	float sr, a, b;
-	cin >> a >> b;
-	sr = (a + b) / 2;
-	cout << sr;
-}
