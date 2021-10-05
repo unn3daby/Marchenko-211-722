765b4c4d08f671dd3d0213324911886ea24c5c85
unn3daby <91330648+unn3daby@users.noreply.github.com>
2021-10-05 15:16:08 +0300

    Update task6.cpp

--- Lab6/task6.cpp
+++ Lab6/task6.cpp
@@ -5,7 +5,7 @@ int main() {
 	int a, b;
 	cout "Vvedite A: ";
 	cin >> a;
-	b = pow(a, 4) * pow(a, 2) * pow(a, 2);
+	b = pow(a, 2) * pow(a, 2) * pow(a, 2) * pow(a, 2);
 	cout <<"A8 = "<< b;
 	return 0;
 }
