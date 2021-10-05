003054afc49f39c68ed2c44f4cdf6fe43c7eb478
unn3daby <91330648+unn3daby@users.noreply.github.com>
2021-10-05 15:15:25 +0300

    Update task7.cpp

--- Lab6/task7.cpp
+++ Lab6/task7.cpp
@@ -5,7 +5,7 @@ int main() {
 	double a, b, c;
 	cout << "Vvedite A: ";
 	cin >> b;
-	a = pow(b, 3) * pow(b, 6);
+	a = pow(b, 3) * pow(b, 3) * pow(b, 3);
 	c = pow(b, 2) * pow(b, 2)* pow(b,2);
 	cout << "A15 = " << a * c;
 	return 0;
